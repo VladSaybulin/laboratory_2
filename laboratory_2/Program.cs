0b52a9e71104ea74ba3760135b76b7311feae814
VladSaybulin <84198008+VladSaybulin@users.noreply.github.com>
2022-02-15 19:54:25 +0300

    Rename variable

--- laboratory_2/Program.cs
+++ laboratory_2/Program.cs
@@ -15,12 +15,12 @@ do
     }
 } while (arraySize < 1);
 
-var maxValueArray = (int) Math.Pow(arraySize + 5, 2);
+var maxValue = (int) Math.Pow(arraySize + 5, 2);
 
 var random = new Random(); 
 var array = new float[arraySize];
 for (int i = 0; i < arraySize; i++)
 {
-    array[i] = random.Next() % maxValueArray;
+    array[i] = random.Next() % maxValue;
     Console.Write($"{array[i]} ");
 }
