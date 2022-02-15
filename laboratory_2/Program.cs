5fbf3f489bf76c8e3c20f2b639ce6d2d1c06b6df
VladSaybulin <84198008+VladSaybulin@users.noreply.github.com>
2022-02-15 09:48:42 +0300

    Initial commit

---
+++ laboratory_2/Program.cs
@@ -0,0 +1,26 @@
+﻿var arraySize = 0;
+do
+{
+    try
+    {
+        arraySize = int.Parse(Console.ReadLine()!);
+        if (arraySize < 1)
+        {
+            Console.WriteLine("Введите число больше 0");
+        }
+    }
+    catch (OverflowException)
+    {
+        Console.WriteLine("Вы ввели не числовое значение");
+    }
+} while (arraySize < 1);
+
+var maxValueArray = (int) Math.Pow(arraySize + 5, 2);
+
+var random = new Random(); 
+var array = new float[arraySize];
+for (int i = 0; i < arraySize; i++)
+{
+    array[i] = random.Next() % maxValueArray;
+    Console.Write($"{array[i]} ");
+}
